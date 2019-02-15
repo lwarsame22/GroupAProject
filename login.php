93fe1b714f40cd6f249dc815861faeb451d75e73
Abdoul94 <1812813@rgu.ac.uk>
2019-02-15 16:26:42 +0000

    ab

--- login.php
+++ login.php
@@ -1,7 +1,44 @@
-<?php
-session_start(); // create or retrieve session
-if (!IsSet($_SESSION["username"]))
-{header("Location:")}
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Multilevel Login Form </tilte>
+        </head>
+<body>
+<form method="POST">
+    <table>
+        <tr>
+            <td>User Type</td>
+            <td><select name="type">
+                    <option value="1"select user type</option>
+                    <option value="Admin">Admin </option>
+                    <option value="">User</option>
+                </select></td>
+        </tr>
+        <tr> Username</tr>
+        <td><input type="text" name="Username" placeholder="Username"></td>
+        </tr>
+
+        <tr>
+
+            <td>Password</td>
+            <td>input type="password" name="pwd" placeholder="password></td>
+        </tr>
+        <tr>
+
+
+            <td><input type="password" name="pwd" placeholder="password"></td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td><input> type="submit" name="submit" value ="Login"></td>
+        </tr>
+
+    </table>
+</form>
+
+</body>
+</html>
 /**
  * Created by PhpStorm.
  * User: 1812315
