f0ea0a7791ac8e1df815bd5ff8949581bf30cfd6
shaunbuchan <1812315@rgu.ac.uk>
2019-02-15 14:17:38 +0000

    edit

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+session_start(); // create or retrieve session
+if (!IsSet($_SESSION["username"]))
+{header("Location:")}
+/**
+ * Created by PhpStorm.
+ * User: 1812315
+ * Date: 15/02/2019
+ * Time: 14:05
+ */
