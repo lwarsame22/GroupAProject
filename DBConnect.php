90f31c0bb1a7579d0ad13b9431aafa660b838798
pams19 <1810356@rgu.ac.uk>
2019-02-18 12:23:24 +0000

    profile html

---
+++ DBConnect.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1810356
+ * Date: 18/02/2019
+ * Time: 11:58
+ */
+
+
+$servername = "CSDM-WEBDEV";
+$dbname="db1810356_qvan";
+$username = "1810356";
+$password = "Molly_199123";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
