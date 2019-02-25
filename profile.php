63d75beb8c9a92b661f5d37b547c3c01674af72b
shaunbuchan <1812315@rgu.ac.uk>
2019-02-25 14:33:30 +0000

    edit profile

---
+++ profile.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Profile</title>
+    <link rel="stylesheet" href="css/homeStyle.css">
+</head>
+<body>
+<form action="#">
+    <label for="firstname">First name:</label><br>
+    <Input type="text" placeholder="Enter First name" name="firstname" required><br>
+    <label for="lastname">Last name:</label><br>
+    <input type="text" placeholder="Enter Last name" name="last name" required><br>
+    <input type="radio">gender:
+    <label for="address">Address:</label><br>
+    <Input type="text" placeholder="Enter Address" name="address" required><br>
+    <label for="city">City:</label><br>
+    <input type="text" placeholder="Enter City" name="city"><br>
+    <label for="country">Country</label><br>
+    <Input type="text" placeholder="Enter Country" name="country" required><br>
+    <label for="datebirth">Date Of Birth:</label><br>
+    <input type="date" placeholder="Enter Date Of Birth" name="datebirth"><br>
+    <label for="mobile">Mobile Number:</label><br>
+    <Input type="tel" placeholder="Enter Mobile Number" name="mobile" required><br>
+
+    <br><br>
+
+    <input type="submit">
+</form>
+</body>
+</html>
