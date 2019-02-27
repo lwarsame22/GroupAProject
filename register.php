c1cc810e48b517310ddba76084031afb5303af12
shaunbuchan <1812315@rgu.ac.uk>
2019-02-27 15:03:30 +0000

    edit

--- register.php
+++ register.php
@@ -18,13 +18,17 @@ session_start();
         $result = mysqli_query($conn, $query);
 
         if ($result) {
-            $query_2 = "INSERT INTO user_profile(u_mail)VALUES('$email') WHERE user_validation.u_ID=user_profile.u_ID";
-            $result_2 = mysqli_query($conn, $query_2);
-            if($result_2){
+            $userID= mysqli_insert_id();
+
+            $IDrequest= mysqli_query($conn, $userID);
+            $query_2 = "INSERT INTO user_profile(u_ID) VALUES('$userID')";
+            $result_2= mysqli_query($conn, $query_2);
+            $query_3 = "UPDATE user_profile SET u_mail=$email WHERE user_validation.u_ID=user_profile.u_ID";
+            $result = mysqli_query($conn, $query_3);
+
             echo "Succesfully registered";
             $_SESSION['username'] = $username;
             header('Location: user.php'); //NEED TO EDIT THIS LATER
-            }
         } else {
 
             echo "Failed to register";
