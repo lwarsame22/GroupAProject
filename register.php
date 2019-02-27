ef56c5c80fe8f1e4004d1a1f80caffc2c6354640
shaunbuchan <1812315@rgu.ac.uk>
2019-02-27 15:07:58 +0000

    edit

--- register.php
+++ register.php
@@ -23,8 +23,7 @@ session_start();
             $IDrequest= mysqli_query($conn, $userID);
             $query_2 = "INSERT INTO user_profile(u_ID) VALUES('$userID')";
             $result_2= mysqli_query($conn, $query_2);
-            $query_3 = "UPDATE user_profile SET u_mail=$email WHERE user_validation.u_ID=user_profile.u_ID";
-            $result = mysqli_query($conn, $query_3);
+         
 
             echo "Succesfully registered";
             $_SESSION['username'] = $username;
