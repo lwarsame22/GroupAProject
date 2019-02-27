db4f8ac8f61b346f61849c961dfa24287e67dacf
shaunbuchan <1812315@rgu.ac.uk>
2019-02-27 18:06:29 +0000

    edit

--- register.php
+++ register.php
@@ -19,15 +19,15 @@ session_start();
         $query = "INSERT INTO user_validation(v_mail, username, password) 
           VALUES('$email', '$username', '$password')";
 
-
         $result = mysqli_query($conn, $query);
 
         if ($result) {
-            $id = mysqli_insert_id($conn);
+            $userID = mysqli_insert_id($conn);
             $query2= "INSERT INTO user_profile(u_name, u_lastname, u_mail, gender, address, city, country, datebirth, mobilenum, active, u_ID, p_ID)
-            VALUES ('NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', '$id', 'NULL')";
+            VALUES ('NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', '$userID', 'NULL')";
             $result2 = mysqli_query($conn, $query2);
-            echo " last nserted id ". $id;
+
+            echo " last nserted id ". $userID;
 
             $_SESSION['username'] = $username;
            // header('Location: user.php'); //NEED TO EDIT THIS LATER
