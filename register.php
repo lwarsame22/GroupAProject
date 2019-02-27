f711a6d8ce36dd0940607822c29445c7433c425c
shaunbuchan <1812315@rgu.ac.uk>
2019-02-27 14:47:23 +0000

    edit

--- register.php
+++ register.php
@@ -12,18 +12,19 @@ session_start();
     // $pass= md5($password);
 
         $query = "INSERT INTO user_validation(v_mail, username, password) 
-          VALUES('$email', '$username', '$password'),
-          Insert INTO user_profile(u_mail) VALUES ('$email')";
+          VALUES('$email', '$username', '$password')";
 
 
         $result = mysqli_query($conn, $query);
 
         if ($result) {
-
+            $query_2 = "INSERT INTO user_profile(u_mail)VALUES('$email') WHERE user_validation.u_ID=user_profile.u_ID";
+            $result_2 = mysqli_query($conn, $query_2);
+            if($result_2){
             echo "Succesfully registered";
             $_SESSION['username'] = $username;
             header('Location: user.php'); //NEED TO EDIT THIS LATER
-
+            }
         } else {
 
             echo "Failed to register";
