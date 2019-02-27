f676e33ada9de7da8904a484d149cccf1b09b294
shaunbuchan <1812315@rgu.ac.uk>
2019-02-27 16:41:51 +0000

    edit

--- register.php
+++ register.php
@@ -17,16 +17,15 @@ session_start();
 
         $query = "INSERT INTO user_validation(v_mail, username, password) 
           VALUES('$email', '$username', '$password')";
-       // $userID= last_insert_id();
-       // "INSERT INTO user_profile(u_mail) WHERE user_profile.u_ID=$userID";
+        $last_id = mysqli_insert_id($conn);
+        $query .= "INSERT INTO user_profile(u_ID) VALUES ('$last_id)";
 
-        $result = mysqli_query($conn, $query);
+        $result = mysqli_multi_query($conn, $query);
 
         if ($result) {
-            $last_id = mysqli_insert_id($conn);
+
             echo " last nserted id ". $last_id;
-            $query_2="INSERT INTO user_profile(u_ID) VALUES ('$last_id)";
-            $result_2 = mysqli_query($conn, $query_2);
+
             $_SESSION['username'] = $username;
            // header('Location: user.php'); //NEED TO EDIT THIS LATER
         } else {
