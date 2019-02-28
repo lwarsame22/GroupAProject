24e39a36b39e76381c21f3195984be9bc4e6cfd1
shaunbuchan <1812315@rgu.ac.uk>
2019-02-28 15:52:49 +0000

    edit

--- register.php
+++ register.php
@@ -16,7 +16,7 @@ session_start();
     // $pass= md5($password);
 
 
-        $query = "INSERT INTO user_validation(v_mail, username, password) 
+        $query = "INSERT INTO user_validation(u_mail, username, password) 
           VALUES('$email', '$username', '$password')";
 
         $result = mysqli_query($conn, $query);
@@ -24,18 +24,16 @@ session_start();
         // $result2= mysqli_query($conn, $query2);
 
         if ($result) {
-            $userID = mysqli_insert_id($conn);
-            $SeLECT = "SELECT * FROM user_validation";
-            $result3=mysqli_query($conn, $SeLECT);
 
-            $query2= "INSERT INTO user_profile(u_name, u_lastname, u_mail, gender, address, city, country, datebirth, mobilenum, active, u_ID)
-             VALUES ('NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', '$userID')";
+
+            $query2= "INSERT INTO user_profile(u_mail)
+             VALUES ('$email')";
             $result2 = mysqli_query($conn, $query2);
-                echo $result2;
-            echo " last nserted id ". $userID . Sresult3;
+                
+
 
             $_SESSION['username'] = $username;
-           // header('Location: user.php'); //NEED TO EDIT THIS LATER
+           header('Location: user.php');
         } else {
 
             echo "Failed to register";
