e79d9e5442240846c2d8df7c68a487bc75317a49
shaunbuchan <1812315@rgu.ac.uk>
2019-03-03 19:06:22 +0000

    edit

--- Header.php
+++ Header.php
@@ -3,47 +3,30 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Welcome</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/unsemantic-grid-responsive-tablet.css">
-
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="css/homeStyle.css">
 </head>
 <body>
-<?php
-session_start();
-if (isset($_SESSION['username'])){
-    $username=$_SESSION['username'];
 
 
-} else {
-    header('Location: HomePage.php');
-    die();
-}
-?>
+<header class="control">
 
-
-<?php
-require "Header.php";
-?>
-
-<main class="grid-container">
-
-    <!--Section 1 DESCRIPTION-->
-    <section id="AppDescription" class="grid-100">
-        <h1>    Playmate / Matchy / Sporty/ Kudofy  </h1>
-        <img id= "Sportapp" src="Images/sportapp.jpg" alt="Sports App">
-        <h2> Making it easier to find people around you to organize and play your sport </h2>
-        <p> QUICK PLAYs
-            Smart way to find members willing to play a sport and arrange activities with each other
-            TEAM MANAGEMENT
-            Join one or more teams and schedule your matches in order to no overlap. Feed you app with the amount of minutes you played in the match or if you score some points
-            EVENTS AND COMPETITIONS
-            Create a quick event of event run for a competition, leagues or tournaments.
-            Organize one-off or recurring activities, event or training sessions
-            Accept new entries
-        </p>
+    <section class="left">
+        <h2>Sport Playmate</h2>
     </section>
 
-</main>
+    <section class="right">
+        <form id = "login"  method="post" action="login.php">
+            <input type="text" placeholder="username" name="username" class="inputtxt" required>
+            <input type="password" placeholder="password" name="password" class="inputtxt" required>
+            <button type="submit" value="Login" name="loginButton" class="btn"><br>
+            <input type="checkbox">Keep me logged in
+    <a href="#">Forgotten Password</a>
+        </form>
+    </section>
+</header>
+
+
+
 </body>
 </html>
