1042c1dc8ee0ba63d4ab5b05855029cbec69e875
shaunbuchan <1812315@rgu.ac.uk>
2019-03-03 17:52:30 +0000

    edit

--- Header.php
+++ Header.php
@@ -19,7 +19,7 @@
         <form id = "login"  method="post" action="login.php">
             <input type="text" placeholder="username" name="username" class="inputtxt" required>
             <input type="password" placeholder="password" name="password" class="inputtxt" required>
-            <input type="button" value="Login" class="btn"><br>
+            <button type="submit" value="Login" name="loginButton" class="btn"><br>
             <input type="checkbox">Keep me logged in
             <a href="#">Forgotten Password</a>
         </form>
