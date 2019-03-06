036bbab9f0ffe96e437a89623868ff4ff35093f4
IV-I <47425882+IV-I@users.noreply.github.com>
2019-03-06 18:13:42 +0000

    Updated! Please confirm if it rendered...

--- HeaderLoggedin.php
+++ HeaderLoggedin.php
@@ -10,41 +10,39 @@ $username=$_SESSION['username'];
 <head>
     <meta charset="UTF-8">
     <title>Sign Up</title>
-    <link rel="stylesheet" href="css/homeStyle.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
+    
 <body>
-
-
-<header class="control">
-
+ <header class="control">
+     <h1>Sport Playmate</h1>
     <section >
-
-            <h4>Welcome <?php echo $username ?></h4>
             <nav class="grid-container">
                 <ul>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Games</a></li>
                     <li><a href="profile.php">My Profile</a></li>
                     <li><a href="#">Events</a></li>
-
                 </ul>
             </nav>
-
-
     </section>
-
+   <!-- Main Section Begins -->
+     <section class="echo"> <!-- This class name will enable the styling of output after logging in -->
+         <h4>Welcome <?php echo $username ?></h4>
+     </section>
+     <img src="images/football.jpg">
+     
     <section class="right">
         <form id = "logout"  method="post" action="logout.php">
             <button type="submit" value="Logout" name="logOutButton" class="btn"> Log out <br>
         </form>
     </section>
 </header>
-
-
-
-
-
-
-
+<!-- Header Ends -->
+<!-- Footer Begins -->
+     <?php
+    reguire "Footer.php"
+    ?>
+<!-- Footer Ends -->
 </body>
 </html>
