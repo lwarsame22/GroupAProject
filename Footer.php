2d659f0af9d470f05f7dcd0a067665dbfb25a85f
IV-I <47425882+IV-I@users.noreply.github.com>
2019-03-06 15:09:05 +0000

    Update footer

--- Footer.php
+++ Footer.php
@@ -5,8 +5,21 @@
 
 <!-- Header Start -->
 <footer class="control">
+        <footer class="control">
 
-    <p> (c) 2016 Mike Crabb</p>
+        <h4>Connect and share with a network of sports lovers in your community</h4>
+        <!--  <span>
+              <li><a><i class="fab fa-facebook-square"></i></a></li>
+              <l><a href="http://www.facebook.com"><img src="facebooklogo.png" alt="Facebook"</a></l>
+              <li><a href="http://www.twitter.com"><img src="twitterlogo.png" alt="twitter"</a></li>
+              <li><a href="http://www.linkedin.com"><img src="linkedinlogo.png" alt="linkedin"</a></li>
+              <li><a href="http://www.googleplus.com"><img src="googlepluslogo.png" alt="Google+"</a></li>
+              <li><a href="http://www.youtube.com"><img src="youtubelogo.png" alt="YouTube"</a></li>
+          </span>-->
+        <h4>Copyright &copy; 2019. Software Project Engineering (CMM004)</h4>
+        <p>All Rights Reserved</p>
+            <small> Developed and Maintain by <a href="team.html">Group A Team</a></small>
+    </footer>
 
 
 </footer>
