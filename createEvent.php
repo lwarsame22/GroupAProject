0443a7dae7823cdfe158a6204b792ca76909f555
shaunbuchan <1812315@rgu.ac.uk>
2019-03-09 19:06:57 +0000

    profile table merged

--- createEvent.php
+++ createEvent.php
@@ -18,7 +18,7 @@ if ($_POST['eventButton']) {
 
 // $pass= md5($password);
     $query = "INSERT INTO events(e_username, e_title, e_description, e_location)
-          VALUES ('$username','b','b','b')";
+          VALUES ('$username','$title','$description','$location')";
 
 
     $result = mysqli_query($conn, $query);
