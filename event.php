f18457bb4d0d23023d68ef7303bef3f9254b252a
shaunbuchan <47092882+shaunbuchan@users.noreply.github.com>
2019-03-09 23:13:24 +0000

    Update event.php

--- event.php
+++ event.php
@@ -12,9 +12,7 @@ session_start();
 if ( isset($_SESSION['username'] )){
     $username=$_SESSION['username'];
 
-    $query2 = "SELECT s_name FROM sports";
-    $result=mysqli_query($conn, $query2);
-    $row=mysqli_fetch_array($result);
+    
     require "HeaderLoggedin.php";
 }
 else {
@@ -31,10 +29,17 @@ else {
     <input type="text" placeholder="Enter Description" name="description"><br>
     <label for="location">Location : </label><br>
     <input type="text" placeholder="Enter location" name="location"><br>
-    <?php
-    while($row) {
-        echo "Works" .$row['s_name']. ".$query2";
-    } ?>
+    <select class="sportdropdown" name="sportsList">
+        <?php 
+        $query2 = "SELECT s_name FROM sports";
+        $result=mysqli_query($conn, $query2);
+        $row=mysqli_fetch_array($result);
+        $menu="";
+        while($row) {
+        $menu.="<option value=".$row['s_name'].">" . $row['s_name'] . "</option>";
+        } echo $menu; 
+        ?>
+        </select>
     <button type="submit" value="createEvent" name="eventButton" >Create Event</button><br>
 </form>
 </body>
