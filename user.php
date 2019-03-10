7db4c599bbd18b13b357ccb9b0cc38cbfdb114a1
IV-I <47425882+IV-I@users.noreply.github.com>
2019-03-10 19:29:37 +0000

    Update user.php

--- user.php
+++ user.php
@@ -76,10 +76,10 @@ else {
     </section>
     <!-- End of Main -->
 
+<!-- Footer -->
+  <!--  <footer class="control"> -->
 
-    <footer class="control">
-
-<h4>Connect and share with a network of sports lovers in your community</h4>
+<!-- <h4>Connect and share with a network of sports lovers in your community</h4>
      <!--  <span>
            <li><a><i class="fab fa-facebook-square"></i></a></li>
            <l><a href="http://www.facebook.com"><img src="facebooklogo.png" alt="Facebook"</a></l>
@@ -88,17 +88,12 @@ else {
            <li><a href="http://www.googleplus.com"><img src="googlepluslogo.png" alt="Google+"</a></li>
            <li><a href="http://www.youtube.com"><img src="youtubelogo.png" alt="YouTube"</a></li>
        </span>-->
-<h4>Copyright &copy; 2019. Software Project Engineering (CMM004)</h4>
+<!-- <h4>Copyright &copy; 2019. Software Project Engineering (CMM004)</h4>
     <p>All Rights Reserved</p>
     <small> Developed and Maintain by <a href="#">Group A Team</a></small>
-</footer>
+</footer> -->
 
-<!-- Footer Ends -->
+ <!-- Footer Ends -->
 
 </body>
-
-<!-- Begin Footer -->
-<!-- <?php
-require "Footer.php";
-?> -->
 </html>
