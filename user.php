0c440d4fa2574d1b812182a481ce09d2daf567b0
shaunbuchan <1812315@rgu.ac.uk>
2019-03-20 14:10:15 +0000

    removed session start

--- user.php
+++ user.php
@@ -14,9 +14,9 @@
 <!--This PHP IF logic is using a choosing a different header depending if the user is logged in or not -->
 
 <?php
-
+session_start();
 if ( isset($_SESSION['username'] )){
-
+    $username=$_SESSION['username'];
 
     require "HeaderLoggedin.php";
 }
