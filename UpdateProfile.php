1798e43f9dd233382737d4030cc0333ab36cd5c6
pams19 <1810356@rgu.ac.uk>
2019-03-21 17:58:56 +0000

    img

--- UpdateProfile.php
+++ UpdateProfile.php
@@ -24,7 +24,32 @@ if ($_POST['profileButton']) {
 
     $mobilenumber = strip_tags($_POST['mobilenumber']);
 
+// $pass= md5($password);
+    $query = "UPDATE  user_profile
+          SET u_name= '$uname',
+          u_lastname ='$ulname',
+          gender ='$gender',
+          address ='$address',
+          city ='$city',
+          country ='$country',
+          datebirth='$datebirth',
+          mobilenum='$mobilenumber',
+          active='1'
+          WHERE u_username = '$username'";
 
+
+    $result = mysqli_query($conn, $query);
+
+    if ($result) {
+
+
+        header('Location: user.php'); // need to EDIT THIS LOCATION TO THE PROFILE VIEW PAGE
+    } else {
+
+        echo "Failed to update";
+    }
+
+ //Image query   
     $statusMsg = '';
 
     // File upload path
@@ -67,30 +92,7 @@ echo $statusMsg;
 
 
 
-// $pass= md5($password);
-$query = "UPDATE  user_profile
-          SET u_name= '$uname',
-          u_lastname ='$ulname',
-          gender ='$gender',
-          address ='$address',
-          city ='$city',
-          country ='$country',
-          datebirth='$datebirth',
-          mobilenum='$mobilenumber',
-          active='1'
-          WHERE u_username = '$username'";
 
-
-$result = mysqli_query($conn, $query);
-
-if ($result) {
-
-
-    header('Location: user.php'); // need to EDIT THIS LOCATION TO THE PROFILE VIEW PAGE
-} else {
-
-    echo "Failed to update";
-}
 }
 
 
