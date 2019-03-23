761301eb51353756af1937c2147a54a1f6e2df4d
pams19 <1810356@rgu.ac.uk>
2019-03-23 17:32:57 +0000

    gender pau

--- viewEvent.php
+++ viewEvent.php
@@ -56,19 +56,19 @@
     $row= mysqli_fetch_assoc($result);
     // maybe this is a for loop because there will be several results since it retrieves all
     // of the event rows
-   // $row= mysqli_fetch_assoc($result)
-        //while ($row= mysqli_fetch_assoc($result)){
+   $row= mysqli_fetch_assoc($result)
+        while ($row= mysqli_fetch_assoc($result)){
 
-          //  $eventID=$row['e_ID']    ;
-          //  $creator=$row['e_username'];
-          //  $eventTitle=$row['e_title'];
-           // $eventDescription=$row['e_description'];
-           // $location=$row['e_location'];
-           // $date=$row['e_date'];
-            //$sportID=$row['e_sportID'];
+            $eventID=$row['e_ID']    ;
+            $creator=$row['e_username'];
+            $eventTitle=$row['e_title'];
+            $eventDescription=$row['e_description'];
+           /$location=$row['e_location'];
+           /$date=$row['e_date'];
+            $sportID=$row['e_sportID'];
 
-       // }
-   // }
+        }
+    }
     ?>
 
     <div class="viewEventTable">
