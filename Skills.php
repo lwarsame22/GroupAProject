9b86f03e680e5763f47ae7e5a5d1944e79936192
pams19 <1810356@rgu.ac.uk>
2019-04-06 17:49:49 +0100

    Level skills

--- Skills.php
+++ Skills.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
    <meta charset="UTF-8">
-    <title>Profile</title>
+    <title>Profiiile</title>
     <link rel="stylesheet" href="css/pstyle.css">
   <style>
        body{
