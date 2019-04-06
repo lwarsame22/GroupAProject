7c9e67e0680ebc8f2d343b203aeff4044ae21aa8
shaunbuchan <1812315@rgu.ac.uk>
2019-04-06 17:52:21 +0100

    join

--- joinEvent.php
+++ joinEvent.php
@@ -2,15 +2,15 @@
 session_start();
 
 include_once('DBConnect.php');
-if ($_POST['joinButton']) {
-
+//if ($_POST['joinButton']) {
+$e_ID = $_POST['eid'];
     $username = $_SESSION['username'];
-    $e_ID = $_GET['eid'];
+    //$e_ID = $_GET['eid'];
 
     $query = "INSERT INTO join_event(j_username, j_event) VALUES('$username','$e_ID')";
     $result = mysqli_query($conn, $query);
     if ($result) {
         header('Location: viewEventdetails.php?eid=' . $e_ID);
     }
-}
+//}
 ?>
