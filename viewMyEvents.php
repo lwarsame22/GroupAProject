3b532f10aec3da3e8382c3f1d1c0549943697bc7
pams19 <1810356@rgu.ac.uk>
2019-04-24 18:06:24 +0100

    style prof

--- viewMyEvents.php
+++ viewMyEvents.php
@@ -1,69 +1,147 @@
+
+
+<?php
+session_start();
+include_once('DBConnect.php');
+if(isset($_SESSION['username'])) {
+    $username=$_SESSION['username'];
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Event</title>
-    <link rel="stylesheet" href="css/style.css">
-    <style>
-        main{
-            background: url("images/updateprofile.jpg");
-        }
-    </style>
-    <style>
-        main {
-            height: 700px;
-        }
-        .viewEventTable{
 
-            color: white;
-            font-size: 18px;
-            font-family: Arial, sans-serif;
-            font-weight: bold;
-        }
-        h2{
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-            color: white;
-            font-size: 22px;
-            font-family: Arial, sans-serif;
-            font-weight: bold;
-        }
+    <title>PlayMate</title>
 
-        a{
-            text-decoration: none;
-            color: turquoise;
-        }
-        th{
-            text-align: center;
-        }
-        table, td{
-            text-align: left;
-        }
-        table, th, td {
-            border: 1px solid turquoise;
-            border-collapse: collapse;
-        }
+    <!--  core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    </style>
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/grayscale.min.css" rel="stylesheet">
+
 </head>
-<body>
-<?php
-
-    require_once ('HeaderLoggedin.php');
 
 
-?>
+<body id="page-top">
 
-<!-- Events fgI am creating  class="grid-container"-->
+<!-- Navigation -->
 
-<main >
-    <section  class="grid-container2">
-        <div class="grid-45">
-            <h2> Events I have created </h2>
-    <?php
-    $query2="SELECT * FROM events WHERE events.e_username='$username'AND events.e_date>=CURDATE() ";
-    $result=mysqli_query($conn, $query2);
-    
-    $events="";
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+            <section class="echo"> <!-- This class name will enable the styling of output after logging in -->
+                <h4>Welcome <?php echo $username; ?></h4>
+            </section></a>
+
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars"></i>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+
+            <div class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="user.php">Home</a>
+                    <div class="dropdown-content">
+                        <a href="#"></a><br>
+                        <a href="#"> </a><br>
+                        <a href="#"></a><br>
+                    </div>
+                </li>
+            </div>
+
+            <div class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="viewProfile.php">Profile</a>
+                    <div class="dropdown-content">
+                        <a href="profile.php">Edit Profile</a><br>
+                        <a href="Skills.php">Edit Skills</a><br>
+                        <a href="#"></a><br>
+                    </div>
+                </li>
+            </div>
+
+
+            <div class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="Inbox.php">Messages</a>
+                    <div class="dropdown-content">
+                        <a href="Inbox.php">My Inbox</a><br>
+                        <a href="outbox.php">My Outbox</a><br>
+                        <a href="#"></a><br>
+                    </div>
+                </li>
+            </div>
+
+            <div class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="event.php">Event</a>
+                    <div class="dropdown-content">
+                        <a href="viewallEvents.php">All Events</a><br>
+                        <a href="viewMyEvents.php">My Events </a><br>
+                        <a href="event.php">Create Event</a><br>
+                    </div>
+                </li>
+            </div>
+            <div class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="searchBySport.php">Search</a>
+                    <div class="dropdown-content">
+                        <a href="searchForUser.php">By user</a><br>
+                        <a href="searchByLocation.php">By location</a><br>
+                        <a href="searchBySport.php">By sport</a><br>
+                    </div>
+                </li>
+            </div>
+
+
+            <section class="right">
+                <form id = "logout"  method="post" action="logout.php">
+                    <button type="submit" value="Logout" name="logOutButton" class="btn-primary"> Log out <br>
+                </form>
+            </section>
+        </div>
+
+    </div>
+</nav>
+
+<!-- Header content-control -->
+<header class="masthead ">
+    <div class="container d-flex h-100 align-items-center">
+        <div class=" signup-section mx-auto text-center">
+            <h1 class="mx-auto my-0 text-uppercase">Events</h1>
+            <br>
+            <!-- Events fgI am creating  class="grid-container"-->
+
+
+
+        </div>
+
+
+
+    </div>
+</header>
+
+<section  class="grid-container2 ">
+    <div class="grid-45">
+        <h2> Events I have created </h2>
+        <?php
+        $query2="SELECT * FROM events WHERE events.e_username='$username'AND events.e_date>=CURDATE() ";
+        $result=mysqli_query($conn, $query2);
+
+        $events="";
         if (mysqli_num_rows($result) > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
                 $e_ID = $row['e_ID'];
@@ -77,49 +155,104 @@
             echo "<p>You have not created an Event yet</p>";
         }
         ?>
-</div>
+    </div>
 
     <!-- Events I am creating  </section> <section >class="grid-container"-->
 
-        <div  class="grid-45">
-            <h2> Events I am attending </h2>
-    <?php
-    $query3="SELECT * 
+    <div  class="grid-45">
+        <h2> Events I am attending </h2>
+        <?php
+        $query3="SELECT * 
             FROM join_event j, events e 
             WHERE j.j_username='$username'AND e.e_ID =j.j_event AND events.e_date>=CURDATE()";
-            $result2=mysqli_query($conn, $query3);
+        $result2=mysqli_query($conn, $query3);
 
-    $attendingevents="";
-   // if (mysqli_num_rows($result2) > 0) {
+        $attendingevents="";
+        // if (mysqli_num_rows($result2) > 0) {
         if(!$result || mysqli_num_rows($result)== 0){
             echo "<p>You are not attending any Event yet</p>";
         }else{
-        while ($row = mysqli_fetch_assoc($result2)) {
-            $e_ID = $row['e_ID'];
-            $e_title = $row['e_title'];
-            $location = $row['e_location'];
-            $e_date = $row['e_date'];
-            $attendingevents .= "<a href='viewEventDetails.php?eid=" . $e_ID . "' class = 'cat_links'>" . $e_title . " -<br> <font size='-3', color='#778899'>" . $location . "-</font><font class='edate' >.$e_date</font></a>";
-        }
+            while ($row = mysqli_fetch_assoc($result2)) {
+                $e_ID = $row['e_ID'];
+                $e_title = $row['e_title'];
+                $location = $row['e_location'];
+                $e_date = $row['e_date'];
+                $attendingevents .= "<a href='viewEventDetails.php?eid=" . $e_ID . "' class = 'cat_links'>" . $e_title . " -<br> <font size='-3', color='#778899'>" . $location . "-</font><font class='edate' >.$e_date</font></a>";
+            }
 
-        echo $attendingevents;
-    }
+            echo $attendingevents;
+        }
 
 
 
         ?>
 
+    </div>
+</section>
+
+
+
+<!-- Contact Section -->
+<section class="contact-section bg-black">
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-4 mb-3 mb-md-0">
+                <div class="card py-4 h-100">
+                    <div class="card-body text-center">
+                        <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+                        <h4 class="text-uppercase m-0">UK</h4>
+                        <hr class="my-4">
+                        <div class="small text-black-50">RGU Aberdeen </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 mb-3 mb-md-0">
+                <div class="card py-4 h-100">
+                    <div class="card-body text-center">
+                        <i class="fas fa-envelope text-primary mb-2"></i>
+                        <h4 class="text-uppercase m-0">Email</h4>
+                        <hr class="my-4">
+                        <div class="small text-black-50">
+                            <a href="#">playmate@rgu.ac.uk</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 mb-3 mb-md-0">
+                <div class="card py-4 h-100">
+                    <div class="card-body text-center">
+                        <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                        <h4 class="text-uppercase m-0">Phone</h4>
+                        <hr class="my-4">
+                        <div class="small text-black-50">902-8832</div>
+                    </div>
+                </div>
+            </div>
         </div>
-        </section>
 
+        <div class="social d-flex justify-content-center">
+            <a href="#" class="mx-2">
+                <i class="fab fa-twitter"></i>
+            </a>
+            <a href="#" class="mx-2">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" class="mx-2">
+                <i class="fab fa-github"></i>
+            </a>
+        </div>
 
-</main>
-<!--Main Ends -->
+    </div>
+</section>
+
 <!-- Footer -->
-<footer>
-    <?php
-    require ("Footer.php")
-    ?>
+<footer class="bg-black small text-center text-white-50">
+    <div class="container">
+        Copyright &copy; Group A for School Computing Science and Digital Media
+    </div>
 </footer>
-</body>
-</html>
+<!-- Footer Ends -->
--- viewProfile.php
+++ viewProfile.php
@@ -189,16 +189,96 @@ if(isset($_SESSION['username'])) {
 
     </div>
 </header>
-<main>
 
-</main>
-<!--Main Ends -->
+<!-- About Section -->
+<section id="about" class="about-section text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+                <h2 class="text-white mb-4">This is for you, just give it a try</h2>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<!-- Contact Section -->
+<section class="contact-section bg-black">
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-4 mb-3 mb-md-0">
+                <div class="card py-4 h-100">
+                    <div class="card-body text-center">
+                        <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+                        <h4 class="text-uppercase m-0">UK</h4>
+                        <hr class="my-4">
+                        <div class="small text-black-50">RGU Aberdeen </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 mb-3 mb-md-0">
+                <div class="card py-4 h-100">
+                    <div class="card-body text-center">
+                        <i class="fas fa-envelope text-primary mb-2"></i>
+                        <h4 class="text-uppercase m-0">Email</h4>
+                        <hr class="my-4">
+                        <div class="small text-black-50">
+                            <a href="#">playmate@rgu.ac.uk</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 mb-3 mb-md-0">
+                <div class="card py-4 h-100">
+                    <div class="card-body text-center">
+                        <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                        <h4 class="text-uppercase m-0">Phone</h4>
+                        <hr class="my-4">
+                        <div class="small text-black-50">902-8832</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="social d-flex justify-content-center">
+            <a href="#" class="mx-2">
+                <i class="fab fa-twitter"></i>
+            </a>
+            <a href="#" class="mx-2">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" class="mx-2">
+                <i class="fab fa-github"></i>
+            </a>
+        </div>
+
+    </div>
+</section>
+
 <!-- Footer -->
-<footer>
-    <?php
-    require ("Footer.php")
-    ?>
+<footer class="bg-black small text-center text-white-50">
+    <div class="container">
+        Copyright &copy; Group A for School Computing Science and Digital Media
+    </div>
 </footer>
+<!-- Footer Ends -->
+
+
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for this template -->
+<script src="js/grayscale.min.js"></script>
+
 </body>
 </html>
-
